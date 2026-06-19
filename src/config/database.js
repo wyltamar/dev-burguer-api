66f79b8f445fe1bf5a122f3937ebd3fdc5834ef5
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-06-19 11:04:32 -0300

    Configurando o sequelize e o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.export = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: '123456',
+    database: 'dev-burguer-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true
+    } 
+}
