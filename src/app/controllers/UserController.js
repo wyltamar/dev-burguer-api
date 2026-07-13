58762e6462be2d31e6c9f56b90594792f840ce90
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-07-13 19:23:53 -0300

    Validação de email duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,6 +8,18 @@ class UserController{
 
         const { name, email, password_hash, admin} = request.body
 
+        const existintUser = await User.findOne({
+            where: {
+               email
+            }
+        })
+
+        if(existintUser){
+            return response.status(400).json(
+               {message : "Email já está cadastrado, por favor informe um novo email!"}
+            )
+        }
+
          const user = await User.create({
             id: v4(),
             name,
