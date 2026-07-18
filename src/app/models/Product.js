011f43b4c6babfcc4d37e00a760c829f7ccbf64b
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-07-17 21:24:16 -0300

    Configuração da model de produto

---
+++ src/app/models/Product.js
@@ -0,0 +1,18 @@
+import Sequelize, {Model} from 'sequelize'
+
+class Product extends Model{
+    static init(sequelize){
+        super.init({
+            name: Sequelize.STRING,
+            price: Sequelize.INTEGER,
+            category: Sequelize.STRING,
+            path: Sequelize.STRING,
+        },
+        {
+            sequelize,
+            tableName: 'products'
+        })
+    }
+}
+
+export default Product
