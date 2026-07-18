07f50595fdace95a5bd6e3833db10037b974352c
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-07-17 21:00:27 -0300

    Criação do controller de Produtos

--- src/database/migrations/20260717134008-create-products-table.cjs
+++ src/database/migrations/20260717134008-create-products-table.cjs
@@ -36,7 +36,6 @@ module.exports = {
           allowNull: false,
         },
           
-      
       });
      
   },
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import UserController from "./app/controllers/UserController.js";
 import SessionController from "./app/controllers/SessionController.js";
+import ProductController from "./app/controllers/ProductController.js";
 
 const routes = new Router();
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+routes.post('/products', ProductController.store)
 
 
 export default routes
