ab87c201f2c5d5d6247f117a8994c973068a8804
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-07-23 17:54:38 -0300

    Implementando middleware de autenticação

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import authConfig from './../config/auth.js'
+
+const authMiddleware = (request, response, next) => {
+    
+    const authToken = request.headers.autorization
+    
+    if(!authToken) response.status(401).json({Error: "Token not provided!"})
+
+    const token = authToken.split(' ')[1]
+
+    try {
+        jwt.verify(token, authConfig.secret, (error, decoded)=>{
+            if(error){
+                
+                throw Error()
+            }
+
+            request.userId = decoded.id
+            
+        })
+    } catch (error) {
+        console.log(error)
+        return response.status(401).json({Error: "Token is invalid!"})
+    }
+
+    return next()
+
+}
+
+
+export default authMiddleware
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import SessionController from "./app/controllers/SessionController.js";
 import ProductController from "./app/controllers/ProductController.js";
 import multer from 'multer'
 import multerConfig from "./config/multer.cjs";
+import authMiddleware from "./middlewares/auth.js";
 
 const routes = new Router();
 
@@ -11,6 +12,7 @@ const upload = multer(multerConfig)
 
 routes.post('/users', UserController.store)
 routes.post('/session', SessionController.store)
+routes.use(authMiddleware)
 routes.post('/products', upload.single('file') ,ProductController.store)
 routes.get('/products', ProductController.index)
 
