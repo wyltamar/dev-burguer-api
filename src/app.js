d1af8dd0d8228cbfbb88445b1e3fa5fccd56c7e5
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-07-29 15:58:36 -0300

    Correção da posição do app.use(orutes)

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(routes)
 
 app.use('/product-file', fileRouteConfig)
 app.use('/category-file', fileRouteConfig)
 
+app.use(routes)
 
 export default app
