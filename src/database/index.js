b0496ad837f9f85f53820b6eb5bc8e7966b58c46
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-08-04 22:24:35 -0300

    Configurando o banco mongo dd e utilização do orm mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize"
+import mongoose from "mongoose"
 import databaseConfig from "../config/database.cjs"
 import User from "../app/models/User.js"
 import Product from "../app/models/Product.js"
@@ -9,12 +10,17 @@ const models = [User, Product, Category]
 class Database{
     constructor(){
         this.init()
+        this.mongo()
     }
 
     init(){
         this.connection = new Sequelize(databaseConfig)
         models.map((model) => model.init(this.connection))
         .map(model => model.associate && model.associate(this.connection.models))
+    }
+
+    mongo(){
+        this.mongooseConnection = mongoose.connect('mongodb://localhost:27017/devburguer')
     }
 }
 
